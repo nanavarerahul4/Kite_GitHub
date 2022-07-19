a968af637bae896735945cc8e23e3c4ddfb49828
Admin <Admin@hp>
2022-07-19 17:08:46 +0530

    [Rahul]:Added new project

---
+++ src/use_of_login/KitLogin.java
@@ -0,0 +1,13 @@
+package use_of_login;
+
+public class KitLogin 
+{
+	public static void main(String[] args)
+	{
+		System.out.println("Login to app");
+		System.out.println("Enter user name");
+		System.out.println("Enter password");
+		System.out.println("Click on login");
+	}
+
+}
---
+++ src/use_of_logout/KiteLogout.java
@@ -0,0 +1,11 @@
+package use_of_logout;
+
+public class KiteLogout 
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("Click on logout");
+		
+	}
+
+}
